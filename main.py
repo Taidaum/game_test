a0942616e6a715bfd67ea833968a61f6a7e831de
taiji uemura <taijiuemura2002@hotmail.com>
2023-10-10 13:53:13 -0300

    Sprite collision test with mouse pointer

--- main.py
+++ main.py
@@ -2,62 +2,82 @@ import pygame
 
 pygame.init()
 
-#CONSTANTS:
-
+#define screen size
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
-BLACK = (0,0,0,0)
-GREEN = (0,255,0)
-RED = (255,0,0)
 
+#create game window
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-clock = pygame.time.Clock()
+pygame.display.set_caption("Masks")
 
-#IMAGE
-soldier = pygame.image.load('soldier.png').convert_alpha()
+#define colours
+BG = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+WHITE = (255, 255, 255)
 
-#INSIDE GAME
-player = soldier.get_rect()
-obstacle = pygame.Rect((300,150,50,50))
-print(player)
+#create soldier class
+class Soldier(pygame.sprite.Sprite):
+  def __init__(self, x, y):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.image.load("soldier.png").convert_alpha()
+    self.rect = self.image.get_rect()
+    self.rect.topleft = (x, y)
+    self.mask = pygame.mask.from_surface(self.image)
 
+#create bullet class
+class Bullet(pygame.sprite.Sprite):
+  def __init__(self):
+    pygame.sprite.Sprite.__init__(self)
+    self.image = pygame.Surface((10, 10))
+    self.rect = self.image.get_rect()
+    self.image.fill(RED)
+    self.mask = pygame.mask.from_surface(self.image)
+
+  def update(self, colour):
+    pos = pygame.mouse.get_pos()
+    self.rect.center = (pos)
+    self.image.fill(colour)
+
+#hide mouse cursor
+pygame.mouse.set_visible(False)
+
+#create instances of soldier and bullet
+soldier = Soldier(350, 250)
+bullet = Bullet()
+
+#create soldier and bullet groups
+soldier_group = pygame.sprite.Group()
+bullet_group = pygame.sprite.Group()
+
+#add instances to groups
+soldier_group.add(soldier)
+bullet_group.add(bullet)
+
+#game loop
 run = True
-pos = 'parado'
 while run:
-    
-    clock.tick(60)
-    #GAME BACKGROUND
-    screen.fill(BLACK)
-    #COLLISION 
-    col = BLACK
-    if player.colliderect(obstacle):
-        col = RED
-   
-    #DRAW RENDER
-    pygame.draw.rect(screen,col,player)
-    pygame.draw.rect(screen,(0,255,255), obstacle)
-    
-    screen.blit(soldier, player)
-    
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a] == True:
-        player.x -= 2
-        pos = 'para esquerda'
-    elif key[pygame.K_d] == True:
-        player.x += 2
-        pos = 'para direita'
-    elif key[pygame.K_w] == True:
-        player.y -= 2
-        pos = 'para cima'
-    elif key[pygame.K_s] == True:
-        player.y += 2
-        pos = 'para baixo'
-        
-    for event in pygame.event.get():
-        print(pos)
-        if event.type == pygame.QUIT:
-            run = False
-            
-    pygame.display.update()            
-            
+
+  #update background
+  screen.fill(BG)
+
+  if pygame.sprite.spritecollide(bullet, soldier_group, False, pygame.sprite.collide_mask):
+    col = RED
+  else:
+    col = GREEN
+
+  bullet_group.update(col)
+
+  soldier_group.draw(screen)
+  bullet_group.draw(screen)
+
+  #event handler
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      run = False
+
+  #update display
+  pygame.display.flip()
+
 pygame.quit()
